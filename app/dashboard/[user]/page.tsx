d89131c72d8fd649a8826f82a49f9f011699b476
hussein <husseinsedoum@gmail.com>
2025-02-20 18:30:15 +0000

    update 0.5 (test fix 6 )

--- app/dashboard/[user]/page.tsx
+++ app/dashboard/[user]/page.tsx
@@ -1,7 +1,7 @@
 import Dashboard from "../components/dashboard/Dashboard"
 
 const UserDashboardPage = ({ params }: { params: { user: string } }) => {
-  const username = String(params.user)
+  const username = params.user // No need for String() since params.user is already a string
   return <Dashboard username={username} />
 }
 
