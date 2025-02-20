856c212491b09a58f22a23c72b610f2655d6a587
hussein <husseinsedoum@gmail.com>
2025-02-20 23:45:59 +0000

    update 0.6 (minor changes to layout)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,11 @@
 import { useRouter } from "next/navigation"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import { ClipLoader } from "react-spinners"
 import { useAuthStore } from "@/store/authStore"
 import { useEffect } from "react"
 import toastMessage from "@/lib/ToastMessage"
 import { getSession } from "next-auth/react"
+import { Loader2 } from "lucide-react"
 
 export default function Login() {
   const router = useRouter()
@@ -45,14 +45,23 @@ export default function Login() {
     return null
   }
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-lg sm:text-xl md:text-2xl text-center font-bold text-primary">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md p-4 sm:p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-xl sm:text-2xl md:text-3xl text-center font-bold text-primary mb-6">
           Se connecter
         </h1>
-        {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
-        <form onSubmit={handleSubmit} className="mt-6">
-          <div className="mb-4">
+
+        {error && (
+          <p className="mt-2 sm:mt-4 text-sm sm:text-base text-red-500 text-center">
+            {error}
+          </p>
+        )}
+
+        <form
+          onSubmit={handleSubmit}
+          className="mt-4 sm:mt-6 space-y-4 sm:space-y-6"
+        >
+          <div>
             <Input
               name="name"
               type="text"
@@ -60,10 +69,11 @@ export default function Login() {
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
-              className="w-full p-2 border border-primary rounded focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 sm:p-3 text-sm sm:text-base border border-primary rounded focus:outline-none focus:ring-2 focus:ring-primary/50"
             />
           </div>
-          <div className="mb-4">
+
+          <div>
             <Input
               name="password"
               type="password"
@@ -71,22 +81,23 @@ export default function Login() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="w-full p-2 border border-primary rounded focus:outline-none focus:ring focus:ring-blue-300"
+              className="w-full p-2 sm:p-3 text-sm sm:text-base border border-primary rounded focus:outline-none focus:ring-2 focus:ring-primary/50"
             />
           </div>
+
           <Button
             type="submit"
-            className="w-full text-white transition-colors flex items-center justify-center"
+            className="w-full h-10 sm:h-12 text-sm sm:text-base text-white transition-colors flex items-center justify-center"
             disabled={loading}
           >
-            {loading && <ClipLoader size={20} color="white" className="mr-2" />}
+            {loading && <Loader2 className="w-6 h-6 animate-spin" />}
             Se connecter
           </Button>
         </form>
-        <p className="mt-4 text-center text-gray-700">
+
+        <p className="mt-4 sm:mt-6 text-sm sm:text-base text-center text-gray-700">
           Vous avez déjà un compte ?
-          <a href="/register" className="text-primary hover:underline">
-            {" "}
+          <a href="/register" className="text-primary hover:underline ml-1">
             S&apos;inscrire
           </a>
         </p>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,76 +1,103 @@
-'use client'
+"use client"
 
-import { useRouter } from 'next/navigation'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { ClipLoader } from 'react-spinners'
-import { useAuthStore } from '@/store/authStore'
-import { useEffect } from 'react'
-import toastMessage from '@/lib/ToastMessage'
-
+import { useRouter } from "next/navigation"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { useAuthStore } from "@/store/authStore"
+import { useEffect } from "react"
+import toastMessage from "@/lib/ToastMessage"
+import { Loader2 } from "lucide-react"
 
 const Register = () => {
-    const router = useRouter()
-    const { isReady, setIsReady, name, password, error, loading, setName, setPassword, register } = useAuthStore()
+  const router = useRouter()
+  const {
+    isReady,
+    setIsReady,
+    name,
+    password,
+    error,
+    loading,
+    setName,
+    setPassword,
+    register,
+  } = useAuthStore()
 
-    const handleRegister = async (e: React.FormEvent) => {
-        const success = await register(e)
-        if (success) {
-            toastMessage('Inscription réussie!', 'success')
-            router.push('/')
-        }
+  const handleRegister = async (e: React.FormEvent) => {
+    const success = await register(e)
+    if (success) {
+      toastMessage("Inscription réussie!", "success")
+      router.push("/")
     }
+  }
 
-    useEffect(() => {
-        setIsReady(true)
-    }, [setIsReady])
-    if (!isReady) {
-        return null;
-    }
+  useEffect(() => {
+    setIsReady(true)
+  }, [setIsReady])
 
-    return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-                <h1 className="text-2xl font-bold text-center text-gray-700">Créer un compte</h1>
-                {error && <p className="mt-4 text-red-500 text-center">{error}</p>}
-                <form onSubmit={handleRegister} className="mt-6">
-                    <div className="mb-4">
-                        <Input
-                            name="name"
-                            type="text"
-                            placeholder="Name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                            className="w-full p-2 border border-primary rounded focus:outline-none focus:ring focus:ring-blue-300"
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <Input
-                            name="password"
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                            className="w-full p-2 border border-primary rounded focus:outline-none focus:ring focus:ring-blue-300"
-                        />
-                    </div>
-                    <Button
-                        type="submit"
-                        className="w-full p-2 text-white transition-colors"
-                        disabled={loading}
-                    >
-                        {loading && <ClipLoader size={20} color='white' className='mr-2' />}
-                        S&apos;inscrire
-                    </Button>
-                </form>
-                <p className="mt-4 text-center text-gray-700">
-                    Vous avez déjà un compte ? <a href="/login" className="text-primary hover:underline">Se connecter</a>
-                </p>
-            </div>
-        </div>
-    )
+  if (!isReady) {
+    return null
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md p-4 sm:p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-xl sm:text-2xl md:text-3xl font-bold text-center text-primary mb-4 sm:mb-6">
+          Créer un compte
+        </h1>
+
+        {error && (
+          <p className="mt-2 sm:mt-4 text-sm sm:text-base text-red-500 text-center">
+            {error}
+          </p>
+        )}
+
+        <form
+          onSubmit={handleRegister}
+          className="mt-4 sm:mt-6 space-y-4 sm:space-y-6"
+        >
+          <div>
+            <Input
+              name="name"
+              type="text"
+              placeholder="Nom"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="w-full p-2 sm:p-3 text-sm sm:text-base border border-primary rounded focus:outline-none focus:ring-2 focus:ring-primary/50"
+            />
+          </div>
+
+          <div>
+            <Input
+              name="password"
+              type="password"
+              placeholder="Mot de passe"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="w-full p-2 sm:p-3 text-sm sm:text-base border border-primary rounded focus:outline-none focus:ring-2 focus:ring-primary/50"
+            />
+          </div>
+
+          <Button
+            type="submit"
+            className="w-full h-10 sm:h-12 text-sm sm:text-base text-white transition-colors flex items-center justify-center"
+            disabled={loading}
+          >
+            {loading && <Loader2 className="w-6 h-6 animate-spin " />}
+            S&apos;inscrire
+          </Button>
+        </form>
+
+        <p className="mt-4 sm:mt-6 text-sm sm:text-base text-center text-gray-700">
+          Vous avez déjà un compte?{" "}
+          <a href="/login" className="text-primary hover:underline">
+            Se connecter
+          </a>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Register
